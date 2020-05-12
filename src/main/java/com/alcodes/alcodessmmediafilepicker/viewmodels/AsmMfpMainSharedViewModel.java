4b575d285c5d1f91c780052a76cf02eb49bd4e50
Wong Yong Xian <danielwongyx5336@gmail.com>
2020-05-12 10:47:00 +0800

    Add Viewmodel for CustomFilePicker

---
+++ src/main/java/com/alcodes/alcodessmmediafilepicker/viewmodels/AsmMfpMainSharedViewModel.java
@@ -0,0 +1,13 @@
+package com.alcodes.alcodessmmediafilepicker.viewmodels;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class AsmMfpCustomFilePickerViewModel extends AndroidViewModel {
+    
+    public AsmMfpCustomFilePickerViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
