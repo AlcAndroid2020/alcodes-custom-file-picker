e48841c7e6db65b0f9a26be6850c69cfe0ad45f6
cstanALC <cstan.alc@gmail.com>
2020-05-12 17:42:02 +0800

    -Implement all file selection list+mediastore code into view model

--- src/main/java/com/alcodes/alcodessmmediafilepicker/fragments/AsmMfpDocumentPickerXlsFragment.java
+++ src/main/java/com/alcodes/alcodessmmediafilepicker/fragments/AsmMfpDocumentPickerXlsFragment.java
@@ -1,10 +1,8 @@
 package com.alcodes.alcodessmmediafilepicker.fragments;
 
 import android.content.Intent;
-import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.ActionMode;
@@ -17,13 +15,16 @@ import android.view.ViewGroup;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.SearchView;
-import androidx.core.content.FileProvider;
-import androidx.documentfile.provider.DocumentFile;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -32,21 +33,27 @@ import com.alcodes.alcodessmmediafilepicker.R;
 import com.alcodes.alcodessmmediafilepicker.activities.AsmMfpGithubSampleFilePickerActivity;
 import com.alcodes.alcodessmmediafilepicker.adapter.AsmMfpDocumentPickerRecyclerViewAdapter;
 import com.alcodes.alcodessmmediafilepicker.utils.MyFile;
+import com.alcodes.alcodessmmediafilepicker.viewmodels.AsmMfpDocumentViewModel;
 
-import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AsmMfpDocumentPickerXlsFragment extends Fragment implements AsmMfpDocumentPickerRecyclerViewAdapter.DocumentFilePickerCallbacks, SearchView.OnQueryTextListener, MenuItem.OnActionExpandListener {
     View view;
     private RecyclerView recyclerView;
     private ArrayList<MyFile> mFileList = new ArrayList<>();
-    private ArrayList<String> selectedList = new ArrayList<>();//only store selected file uri but can be change
     private android.view.ActionMode mActionMode;
     private AsmMfpDocumentPickerRecyclerViewAdapter mAdapter;
     //for action mode custom search bar
     private EditText CustomSearchBar;
     private Button ClearTextBtn;
     private Boolean isSearching = false;
+    //for viewmodel implement
+    private ArrayList<String> TotalselectedList = new ArrayList<>();
+
+    private AsmMfpDocumentViewModel mDocumentViewModel;
+    private NavController mNavController;
+    private int SelecLimitCount;
 
     public AsmMfpDocumentPickerXlsFragment() {
 
@@ -58,16 +65,14 @@ public class AsmMfpDocumentPickerXlsFragment extends Fragment implements AsmMfpD
         view = inflater.inflate(R.layout.asm_mfp_document_fragment, container, false);
         recyclerView = (RecyclerView) view.findViewById(R.id.pdf_RecyclerView);
 
-        mAdapter = new AsmMfpDocumentPickerRecyclerViewAdapter(getContext(), mFileList, AsmMfpDocumentPickerXlsFragment.this, selectedList.size());
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        recyclerView.setAdapter(mAdapter);
+        initAdapter();
         return view;
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        openDocumentMediaStore();
         setHasOptionsMenu(true);
 
         //for custom search bar
@@ -98,81 +103,105 @@ public class AsmMfpDocumentPickerXlsFragment extends Fragment implements AsmMfpD
 
     }
 
-    private void openDocumentMediaStore() {
-        //document format
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        mDocumentViewModel = new ViewModelProvider(mNavController.getBackStackEntry(R.id.asm_mfp_documentfragment),
+                ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().getApplication())).
+                get(AsmMfpDocumentViewModel.class);
+
+        //get selection list from viewmodel
+        mDocumentViewModel.getSelectionList().observe(getViewLifecycleOwner(), new Observer<ArrayList<String>>() {
+            @Override
+            public void onChanged(ArrayList<String> strings) {
+                if (strings.size() > 0) {
+                    TotalselectedList = strings;
+                    mAdapter.setSelectedCounter(TotalselectedList.size());
+                    mAdapter.notifyDataSetChanged();
+                }
+            }
+        });
+
+        //get user selection limit ,by default is 10item
+
+        mDocumentViewModel.getSelectionLimit().observe(getViewLifecycleOwner(), new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                SelecLimitCount = integer;
+                mAdapter.setSelectLimitCounter(SelecLimitCount);
+                mAdapter.notifyDataSetChanged();
+
+            }
+        });
 
         String xls = MimeTypeMap.getSingleton().getMimeTypeFromExtension("xls");
         String xlsx = MimeTypeMap.getSingleton().getMimeTypeFromExtension("xlsx");
 
 
-        //Table
-        Uri table = MediaStore.Files.getContentUri("external");
-        //Column
-        String[] column = {MediaStore.Files.FileColumns.DATA, MediaStore.Files.FileColumns.SIZE};
-        //Where
-        String where = MediaStore.Files.FileColumns.MIME_TYPE + "=?"
-                + " OR " + MediaStore.Files.FileColumns.MIME_TYPE + "=?";
-        //args
-        String[] args = new String[]{xls, xlsx};
-
-        Cursor fileCursor = getActivity().getContentResolver().query(table, column, where, args, null);
-
-        while (fileCursor.moveToNext()) {
-
-            //your code
-            int dataColumn = fileCursor.getColumnIndex(MediaStore.Files.FileColumns.DATA);
-            int sizeColumn = fileCursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.SIZE);
-
-            String filePath = fileCursor.getString(dataColumn);
-            String size = fileCursor.getString(sizeColumn);
-            Uri uri = Uri.fromFile(new File(filePath));
-            //grant permision for app with package "packegeName", eg. before starting other app via intent
-
-            getActivity().grantUriPermission(getActivity().getPackageName(), uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            //revoke permisions
-            Uri newuri = FileProvider.getUriForFile(getContext(), "com.alcodes.alcodesgalleryviewerdemo.fileprovider", new File(filePath));
-            DocumentFile df = DocumentFile.fromSingleUri(getActivity().getApplicationContext(), newuri);
-            //revokeUriPermission(uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
-
-            MyFile myFile = new MyFile(df.getName(), String.valueOf(newuri), false);
-            myFile.setFileType("XLS");
-            myFile.setFileSize(size);
-            mFileList.add(myFile);
-        }
-        fileCursor.close();
-
-
+        ArrayList<String> FileType = new ArrayList<>();
+        FileType.addAll(Arrays.asList(xls, xlsx));
+        mDocumentViewModel.getFileList(FileType, "XLS").observe(getViewLifecycleOwner(), new Observer<ArrayList<MyFile>>() {
+            @Override
+            public void onChanged(ArrayList<MyFile> myFiles) {
+                if (myFiles.get(0).getFileType() == "XLS") {
+                    mFileList = myFiles;
+                    initAdapter();
+                }
+            }
+        });
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Init navigation component.
+        mNavController = Navigation.findNavController(view);
+    }
 
     @Override
     public void onDocumentSelected(Uri uri) {
-        selectedList.add(String.valueOf(uri));
+        //    selectedList.add(String.valueOf(uri));
+        //update with viewmodel
+        TotalselectedList.add(uri.toString());
+
+        mAdapter.setSelectedCounter(TotalselectedList.size());
         if (mActionMode == null)
             mActionMode = getActivity().startActionMode(mActionModeCallback);
-
-        mActionMode.setTitle(selectedList.size() + "item(s) selected");
-        //update for selection limit which allow user only select 5 item
-        mAdapter.setSelectedCounter(selectedList.size());
+        mActionMode.setTitle(TotalselectedList.size() + "item(s) selected");
+        mDocumentViewModel.setSelectionList(TotalselectedList);
     }
 
     @Override
     public void onDocumentUnSelected(Uri uri) {
-        for (int i = 0; i < selectedList.size(); i++) {
+    /*     for (int i = 0; i < selectedList.size(); i++) {
             if (selectedList.get(i).equals(uri.toString()))
                 selectedList.remove(i);
+        }*/
+        for (int i = 0; i < TotalselectedList.size(); i++) {
+            if (TotalselectedList.get(i).equals(uri.toString()))
+                TotalselectedList.remove(i);
         }
-        if (mActionMode != null)
-            mActionMode.setTitle(selectedList.size() + "item(s) selected");
-
-        if (selectedList.size() == 0 && mActionMode != null)
+        mDocumentViewModel.setSelectionList(TotalselectedList);
+        if (TotalselectedList.size() == 0 && mActionMode != null)
             mActionMode.finish();
-        //update for selection limit which allow user only select 5 item
-        mAdapter.setSelectedCounter(selectedList.size());
+        mAdapter.setSelectedCounter(TotalselectedList.size());
+
+        if (mActionMode != null)
+            mActionMode.setTitle(TotalselectedList.size() + "item(s) selected");
+
+    }
+
+    @Override
+    public int onNullSelectionLimit() {
+        return SelecLimitCount;
     }
 
     private ActionMode.Callback mActionModeCallback = new ActionMode.Callback() {
+        MenuItem unLimitItem;
+
         @Override
         public boolean onCreateActionMode(ActionMode mode, Menu menu) {
             mode.getMenuInflater().inflate(R.menu.asm_mfp_menu_document_file_picker, menu);
@@ -181,6 +210,13 @@ public class AsmMfpDocumentPickerXlsFragment extends Fragment implements AsmMfpD
             checkItem.setVisible(true);
             MenuItem unSelectItem = menu.findItem(R.id.Doc_FilePicker_UnselectAll);
             unSelectItem.setVisible(true);
+            unLimitItem = menu.findItem(R.id.Doc_FilePicker_UnLimitedSelection);
+            unLimitItem.setVisible(true);
+            unLimitItem = menu.findItem(R.id.Doc_FilePicker_UnLimitedSelection);
+
+            if (SelecLimitCount != 99)
+                unLimitItem.setVisible(true);
+
             return true;
         }
 
@@ -195,8 +231,8 @@ public class AsmMfpDocumentPickerXlsFragment extends Fragment implements AsmMfpD
         public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
             if (item.getItemId() == R.id.Doc_FilePicker_DoneSelection) {
                 ArrayList<String> mFileList = new ArrayList<>();
-                for (int i = 0; i < selectedList.size(); i++) {
-                    mFileList.add(selectedList.get(i));
+                for (int i = 0; i < TotalselectedList.size(); i++) {
+                    mFileList.add(TotalselectedList.get(i));
                 }
                 if (mFileList != null) {
                     Intent intent = new Intent(getContext(), AsmGvrMainActivity.class);
@@ -206,17 +242,16 @@ public class AsmMfpDocumentPickerXlsFragment extends Fragment implements AsmMfpD
                 }
             }
             if (item.getItemId() == R.id.Doc_FilePicker_UnselectAll) {
-                selectedList.clear();
-                //update recycler view data and ui
 
-                for (int i = 0; i < mFileList.size(); i++) {
-                    if (mFileList.get(i).getIsSelected())
-                        mFileList.get(i).setIsSelected(false);
-                }
-
-                mAdapter = new AsmMfpDocumentPickerRecyclerViewAdapter(getContext(), mFileList, AsmMfpDocumentPickerXlsFragment.this, selectedList.size());
-                recyclerView.setAdapter(mAdapter);
-
+                //clear m file
+                resetFileList();
+                //reinit adapter /refresh
+                initAdapter();
+                TotalselectedList.clear();
+                //update viewmodel as adapter will update along
+                mDocumentViewModel.setSelectionList(TotalselectedList);
+                mDocumentViewModel.setSelectionLimit(SelecLimitCount);
+                Toast.makeText(getContext(), mAdapter.getSelectLimitCounter() + " test", Toast.LENGTH_SHORT).show();
                 //close actionmode
                 mActionMode.finish();
             }
@@ -235,6 +270,11 @@ public class AsmMfpDocumentPickerXlsFragment extends Fragment implements AsmMfpD
                     isSearching = false;
                 }
             }
+            if (item.getItemId() == R.id.Doc_FilePicker_UnLimitedSelection) {
+                mDocumentViewModel.setSelectionLimit(99);
+                unLimitItem.setVisible(false);
+            }
+
 
             return true;
         }
@@ -245,20 +285,28 @@ public class AsmMfpDocumentPickerXlsFragment extends Fragment implements AsmMfpD
             CustomSearchBar.setVisibility(View.INVISIBLE);
             ClearTextBtn.setVisibility(View.INVISIBLE);
             isSearching = false;
-            selectedList.clear();
+            //  selectedList.clear();
 
             //for refresh the view
 
-            for (int i = 0; i < mFileList.size(); i++) {
-                if (mFileList.get(i).getIsSelected())
-                    mFileList.get(i).setIsSelected(false);
-            }
 
-            mAdapter = new AsmMfpDocumentPickerRecyclerViewAdapter(getContext(), mFileList, AsmMfpDocumentPickerXlsFragment.this, selectedList.size());
-            recyclerView.setAdapter(mAdapter);
+            resetFileList();
+            initAdapter();
             mActionMode = null;
         }
     };
+
+    private void resetFileList() {
+        for (int i = 0; i < mFileList.size(); i++) {
+            if (mFileList.get(i).getIsSelected())
+                mFileList.get(i).setIsSelected(false);
+        }
+    }
+
+    private void initAdapter() {
+        mAdapter = new AsmMfpDocumentPickerRecyclerViewAdapter(getContext(), mFileList, AsmMfpDocumentPickerXlsFragment.this);
+        recyclerView.setAdapter(mAdapter);
+    }
 
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
