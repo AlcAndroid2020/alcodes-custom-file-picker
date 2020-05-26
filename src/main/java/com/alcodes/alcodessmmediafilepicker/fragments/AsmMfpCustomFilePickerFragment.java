18b3bedbd212a4b49366bd127ede623e689d8c7c
Loh Chee Chuan <winsonlohcc.98@outlook.com>
2020-05-26 17:49:04 +0800

    > Changed to ImageView (Checked) instead of material checkbox in custom file picker. To make it consistent with Document file picker. Also, realign the checked image in document picker to center. > Searching for any unresolved bugs. Found that document file picker does not have file selection limit (no limit) even though the value was passed from the custom file picker. Using Timber to check the value has passed to document picker successfully. The result shows the preset max file selection value in console log but it still can select more than the preset value. Then, I found out that the fragment only get the extra value from the intent and the value received does not stored into the view model.

--- src/main/java/com/alcodes/alcodessmmediafilepicker/fragments/AsmMfpCustomFilePickerFragment.java
+++ src/main/java/com/alcodes/alcodessmmediafilepicker/fragments/AsmMfpCustomFilePickerFragment.java
@@ -583,7 +583,7 @@ public class AsmMfpCustomFilePickerFragment extends Fragment
             mDataBinding.linearLayoutNoFilesFound.setVisibility(View.GONE);
             mDataBinding.simpleProgressBar.setVisibility(View.VISIBLE);
             mBeenDirectedToDocumentPicker = true;
-            
+
             if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
                 //Android 10 and above
                 Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
--- src/main/java/com/alcodes/alcodessmmediafilepicker/fragments/AsmMfpDocumentPickerMergedFileTypeFragment.java
+++ src/main/java/com/alcodes/alcodessmmediafilepicker/fragments/AsmMfpDocumentPickerMergedFileTypeFragment.java
@@ -201,6 +201,7 @@ public class AsmMfpDocumentPickerMergedFileTypeFragment extends Fragment impleme
             }
         });
         mMaxFileSelection = requireActivity().getIntent().getExtras().getInt(EXTRA_INT_MAX_FILE_SELECTION, 0);
+        mDocumentViewModel.setSelectionLimit(mMaxFileSelection);
         if (mMaxFileSelection != 0)
             isLimited = true;
         //get user selection limit ,by default is 10item
@@ -355,9 +356,7 @@ public class AsmMfpDocumentPickerMergedFileTypeFragment extends Fragment impleme
             mDocumentViewModel.setSelectionList(TotalselectedList);
             initAdapter();
         }
-        if (item.getItemId() == R.id.Doc_FilePicker_SetSelectLimit) {
-            PromptLimitDialog();
-        }
+
         if (item.getItemId() == R.id.Doc_FilePicker_UnselectAll) {
             resetFileList();
             initAdapter();
@@ -374,9 +373,6 @@ public class AsmMfpDocumentPickerMergedFileTypeFragment extends Fragment impleme
             }
 
         }
-        if (item.getItemId() == R.id.Doc_FilePicker_SetSelectLimit) {
-            PromptLimitDialog();
-        }
         if (item.getItemId() == R.id.Doc_FilePicker_DoneSelection) {
             ArrayList<String> mFileList = new ArrayList<>();
             for (int i = 0; i < TotalselectedList.size(); i++) {
@@ -389,42 +385,6 @@ public class AsmMfpDocumentPickerMergedFileTypeFragment extends Fragment impleme
             }
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    private void PromptLimitDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
-        builder.setTitle(getResources().getString(R.string.LimitFileSelection));
-        final EditText input = new EditText(getContext());
-        input.setHint(getResources().getString(R.string.selectionnumber));
-
-        builder.setView(input);
-        builder.setMessage(getResources().getString(R.string.NoLimit));
-
-        builder.setPositiveButton(getResources().getString(R.string.Okay), new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                mDocumentViewModel.setSelectionLimit(Integer.valueOf(input.getText().toString()));
-                mMaxFileSelection = Integer.valueOf(input.getText().toString());
-                mAdapter.setSelectLimitCounter(mMaxFileSelection);
-
-                //if limited again then hide select all option
-                if (mMaxFileSelection != 0)
-                    isLimited = true;
-                else
-                    isLimited = false;
-                getActivity().invalidateOptionsMenu();
-
-                mDocumentViewModel.setSelectionLimit(mMaxFileSelection);
-            }
-        });
-        builder.setNegativeButton(getResources().getString(R.string.Cancel), new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-
-        builder.show();
     }
 
     @Override
